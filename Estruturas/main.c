1ea0e62cec0031dcb73cc8a39306710e882ba04f
leandroflores <leandroflores7@gmail.com>
2017-01-14 18:20:07 -0200

    First Commit.

---
+++ Estruturas/main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: Leandro
+ *
+ * Created on 14 de Janeiro de 2017, 18:19
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    return (EXIT_SUCCESS);
+}
+
